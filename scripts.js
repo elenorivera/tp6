20d93a608cb2bfc764ac371bb9d1f16b2d76fd79
elenorivera <elenorivera509@gmail.com>
2022-10-06 05:45:04 +0000

    changing css for formatting

--- scripts.js
+++ scripts.js
@@ -1,53 +1,62 @@
 // function to load a file from the URL "fromFile" into the object identified by "whereTo"
 function loadFileInto(fromFile, whereTo) {
 
-	// creating a new XMLHttpRequest object
-	ajax = new XMLHttpRequest();
+  // creating a new XMLHttpRequest object
+  ajax = new XMLHttpRequest();
 
-	// defines the GET/POST method, source, and async value of the AJAX object
-	ajax.open("GET", fromFile, true);
+  // defines the GET/POST method, source, and async value of the AJAX object
+  ajax.open("GET", fromFile, true);
 
-	// provides code to do something in response to the AJAX request
-	ajax.onreadystatechange = function() {
-			if ((this.readyState == 4) && (this.status == 200)) {
-				document.querySelector(whereTo).innerHTML += this.responseText;
-				
-			} else if ((this.readyState == 4) && (this.status != 200)) {
-				console.log("Error: " + this.responseText);
-			}
-		
-	} // end ajax.onreadystatechange function
+  // provides code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      document.querySelector(whereTo).innerHTML += this.responseText;
 
-	// initiate request and wait for response
-	ajax.send();
+    } else if ((this.readyState == 4) && (this.status != 200)) {
+      console.log("Error: " + this.responseText);
+    }
+
+  } // end ajax.onreadystatechange function
+
+  // initiate request and wait for response
+  ajax.send();
 
 }
 
 
 window.onload = function() {
-  
+
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
+
   document.querySelector("#titlebanner h1").classList.add("tp");
-  
-  document.querySelector("#titlebanner h1").onclick = function() {    
+
+  document.querySelector("#titlebanner h1").onclick = function() {
     this.classList.toggle("tan");
   }
-  
+
   document.querySelector("#columns #ingredients").onclick = function() {
     this.classList.toggle("tpShow")
   }
-  
+
   document.querySelector("#columns #equipment").onclick = function() {
     this.classList.toggle("tpShow")
   }
-  
+
   document.querySelector("#columns #directions").onclick = function() {
     this.classList.toggle("tpShow")
   }
-  
-  document.querySelector("#columns #directions ol").innerHTML += "<li>Enjoy as you please!</li>";
-  
+
+
+
+
+
   setTimeout(function() {
-		document.querySelector("#columns #ingredients ul").innerHTML += "<li>A hunger for cake</li>";
-	}, 100);
-  
-} 
+    document.querySelector("#columns #ingredients ul").innerHTML += "<li>A hunger for cake</li>";
+    document.querySelector("#columns #directions ol").innerHTML += "<li>Enjoy!</li>";
+  }, 100);
+
+
+
+}
