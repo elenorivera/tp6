8328116dfa5cf6a2a42561c5d74693b67020a8d3
elenorivera <elenorivera509@gmail.com>
2022-10-06 04:46:26 +0000

    new html files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
+// function to load a file from the URL "fromFile" into the object identified by "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+
 window.onload = function() {
   
   document.querySelector("#titlebanner h1").classList.add("tp");
@@ -20,5 +46,8 @@ window.onload = function() {
   
   document.querySelector("#columns #directions ol").innerHTML += "<li>Enjoy as you please!</li>";
   
+  setTimeout(function() {
+		document.querySelector("#columns #ingredients ul").innerHTML += "<li>A hunger for cake</li>";
+	}, 100);
   
 } 
